4a1949899dfcc87d3846bbe0310c93d34c424531
killergator5 <Benferguson4@live.co.uk>
2014-02-25 14:47:39 +0000

    Create morenumbers.py

---
+++ morenumbers.py
@@ -0,0 +1,15 @@
+from datetime import date as dat 
+
+def getYear():
+    year = input (1998)
+    year = int (year)
+    return age
+    
+def age(yearOfBirth
+    today = dat.today ()
+    thisYear = today.year
+    age = thisYear - yearOfBirth
+    return age 
+    
+year = getyear()
+print("yuo will be %s years old this year on your birthday" %age(year))
